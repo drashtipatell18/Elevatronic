4b6ac925361e42f15d4940a232c026d4b2488080
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-05-06 14:34:40 +0530

    forgetpass add

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Mail\ForgotPasswordMail;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use Carbon\Carbon;
 
--- resources/views/auth/password_reset.blade.php
+++ resources/views/auth/password_reset.blade.php
@@ -5,24 +5,24 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Password Reset Link</title>
+    <title>Enlace para restablecer contraseña</title>
 </head>
 
 <body>
-    <p>Hello {{ $user->name }} </p>
+    <p>Hola {{ $user->name }} </p>
 
-    <p>You are receiving this email because we received a password reset request for your account.</p>
+    <p>Está recibiendo este correo electrónico porque recibimos una solicitud de restablecimiento de contraseña para su cuenta.</p>
 
-    <p>Please click the following link to reset your password:</p>
+    <p>Por favor haga clic en el siguiente enlace para restablecer su contraseña:</p>
 
     @component('mail::button', ['url' => url('reset/' . $user->remember_token)])
-        Reset Your Password
+    Restablecer su contraseña
     @endcomponent
 
 
-    <p>If you did not request a password reset, no further action is required.</p>
+    <p>Si no solicitó un restablecimiento de contraseña, no es necesario realizar ninguna otra acción.</p>
 
-    <p>Thank you!</p>
+    <p>¡Gracias!</p>
 </body>
 
 </html>

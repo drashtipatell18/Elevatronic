83482ea689697ec8fc6afd55bf1be416def6e896
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-07-04 12:49:48 +0530

    42 fixed

--- app/Http/Controllers/Elevatortypes/ElevatortypesController.php
+++ app/Http/Controllers/Elevatortypes/ElevatortypesController.php
@@ -41,7 +41,7 @@ class ElevatortypesController extends Controller
     public function elevatortypesDetails($id){
         $elevator_type = Elevatortypes::findOrFail($id);
         $spareparts = SparePart::all();
-        $elevators = Elevators::all();
+        $elevators = Elevators::where('tipo_de_ascensor', $elevator_type->nombre_de_tipo_de_ascensor)->get();
         $assginspares = AssginSpare::where('nombre_del_tipo_de_ascensor', $elevator_type->nombre_de_tipo_de_ascensor)->get();
 
         return view('ElevatorTypes.elevator_details', compact('assginspares','elevators','spareparts','elevator_type'));

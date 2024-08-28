b78699eb8b5c1ee2632c05a36b79e528e476552a
drashtipatell18 <drashti.kalathiyainfotech@gmail.com>
2024-08-28 11:49:34 +0530

    customer provices id pr thi name show

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Province;
 
 class Cliente extends Model
 {
@@ -12,4 +13,7 @@ class Cliente extends Model
     protected $dates = ['eliminado_en'];
     protected $table = 'clientes';
     protected $fillable = ['nombre','tipo_de_cliente','ruc','país','provincia','dirección','teléfono','teléfono_móvil','correo_electrónico','nombre_del_contacto','posición'];
+    public function province() {
+        return $this->belongsTo(Province::class, 'provincia'); // Adjust 'provincia_id' to your actual foreign key
+    }
 }
